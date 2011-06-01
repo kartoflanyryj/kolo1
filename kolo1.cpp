c5355a158dd941365b40b5eec229bcd6001929c5
Lukasz K <kartoflanyryj@gmail.com>
2011-06-01 14:59:45 +0200

    Zakonczono zad 9.

--- kolo1.cpp
+++ kolo1.cpp
@@ -3,8 +3,15 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char **argv) {
-  cout<<"I love git!"<<endl;
-  return 0;
+void pytanie() {
+  cout<<"Q1) Jak wyswietlic roznice miedzy drugim a trzecim zatwierdzeniem wykonanym w galezi master?"<<endl;
+  cout<<"A1) git diff 5672b17 9952ab3"<<endl;
+  cout<<"Q2) Co sie zmienilo?"<<endl;
+  cout<<"A2) zmieniono linie z funkcja main w pliku kolo1.cpp"<<endl;
 }
 
+int main(int argc, char **argv) {
+  cout<<"I love git!"<<endl;
+pytanie();
+  return 0;
+}
